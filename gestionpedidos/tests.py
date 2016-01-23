6ab77f0512326b0d2e9a94393c7d0583e0d9d414
ignaciorecuerda <ignacio.recuerda@gmail.com>
2016-01-23 23:59:39 +0100

    añado mi primer test

--- gestionpedidos/tests.py
+++ gestionpedidos/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from gestionpedidos.models import Cliente, Pedido
+from django.test.client import RequestFactory
 
-# Create your tests here.
+class TestsGestionPedidos(TestCase):
+	def test_nombre_cliente(self):
+		cliente = Cliente.objects.get(slug="Carlos")
+		self.assertEqual(cliente.direccion, 'Spain, Granada, Calle Gran Vía de Colón')
+		print ("direccion correcta")
