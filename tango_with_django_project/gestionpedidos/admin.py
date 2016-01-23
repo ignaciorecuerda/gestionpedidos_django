1488b6697049599c15675b638094dd6e3dc07f34
ignaciorecuerda <ignacio.recuerda@gmail.com>
2016-01-23 20:16:16 +0100

    cambiando la estructura del proyecto

--- tango_with_django_project/gestionpedidos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from gestionpedidos.models import Cliente, Pedido, PerfilUsuario
-
-admin.site.register(Cliente)
-admin.site.register(Pedido)
-admin.site.register(PerfilUsuario)
