ffe858a9c3a49579c3258bd1ef3f125bc35e67b6
ignaciorecuerda <ignacio.recuerda@gmail.com>
2016-01-24 00:08:02 +0100

    actualizo tests

--- gestionpedidos/tests.py
+++ gestionpedidos/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from gestionpedidos.models import Cliente, Pedido
+from gestionpedidos.views import *
 from django.test.client import RequestFactory
 
 class TestsGestionPedidos(TestCase):
