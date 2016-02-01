a9781987c5327ace7a3746c2805499083f951b0f
ignaciorecuerda <ignacio.recuerda@gmail.com>
2016-02-01 22:47:27 +0100

    corrigiendo tests

--- gestionpedidos/tests.py
+++ gestionpedidos/tests.py
@@ -4,7 +4,11 @@ from gestionpedidos.views import *
 from django.test.client import RequestFactory
 
 class TestsGestionPedidos(TestCase):
+
+	def test_crear_cliente(self):
+		Cliente.objects.create(nombre='Test',slug='test',direccion='Pais, ciudad, calle')
+
 	def test_nombre_cliente(self):
-		cliente = Cliente.objects.get(name="Quique")
-		self.assertEqual(cliente.direccion, 'Spain, Granada, recogidas')
+		cliente = Cliente.objects.get(name='Test')
+		self.assertEqual(cliente.direccion, 'Pais, ciudad, calle')
 		print ("direccion correcta")
