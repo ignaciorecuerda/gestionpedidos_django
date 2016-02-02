6ec6fb5e12b9205585a5e063e80fea447e7e613b
ignaciorecuerda <ignacio.recuerda@gmail.com>
2016-02-02 21:30:56 +0100

    para que heroku static

--- gestionpedidos/urls.py
+++ gestionpedidos/urls.py
@@ -11,3 +11,10 @@ urlpatterns = patterns('',
     url(r'^cliente/(?P<cliente_name_slug>[\w\-]+)/$', views.cliente, name='cliente'),
     url(r'^register/$', views.register, name='register'),
     url(r'^reclama_datos/', views.reclama_datos, name='reclama_datos'),)
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
