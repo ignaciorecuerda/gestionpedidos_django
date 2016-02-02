864ff3bbc0e11d2b52004ee56e08ccd8306cbb68
ignaciorecuerda <ignacio.recuerda@gmail.com>
2016-02-02 21:38:42 +0100

    para que heroku static

--- gestionpedidos/urls.py
+++ gestionpedidos/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from gestionpedidos import views
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
