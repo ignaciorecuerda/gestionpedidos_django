ba732e0d5fc3f844738676d44c8ec81f2ec410f3
ignaciorecuerda <ignacio.recuerda@gmail.com>
2016-02-02 22:52:32 +0100

    para servir los staticos en heroku

--- tango_with_django_project/wsgi.py
+++ tango_with_django_project/wsgi.py
@@ -13,5 +13,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tango_with_django_project.setti
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 #from whitenoise.django import DjangoWhiteNoise
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+#application = get_wsgi_application()
 #application = DjangoWhiteNoise(application)
