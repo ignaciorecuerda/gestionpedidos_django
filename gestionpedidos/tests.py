19a123357ecd1f98a834766082c5c4b429abb9c3
ignaciorecuerda <ignacio.recuerda@gmail.com>
2016-02-06 21:39:08 +0100

    actualizo test

--- gestionpedidos/tests.py
+++ gestionpedidos/tests.py
@@ -3,9 +3,9 @@ from gestionpedidos.models import Cliente, Pedido
 from gestionpedidos.views import *
 from django.test.client import RequestFactory
 
+######## Test para clientes ########
 class TestsGestionPedidosClientes(TestCase):
 
-	######## Test para clientes ########
 	def setUp(self):
 		Cliente.objects.create(name='Test',slug='test',direccion='Pais, ciudad, calle')
 
@@ -16,17 +16,20 @@ class TestsGestionPedidosClientes(TestCase):
 		self.assertEqual(cliente.direccion, 'Pais, ciudad, calle')
 		print ("direccion correcta")
 
-#class Test
-	######## test para pedidos ########
-	# def setUp(self):
-	# 	test = Cliente(name='TestTapas',slug='testTapas',direccion='Pais, ciudad, calle')
-	# 	test.save()
-	# 	Pedido.objects.create(cliente=test, title='TestPedido', url="www.test.es", views=1, fechaPedido='02/02/16', precio='1 euro')
+####### test para pedidos ########
+class TestsGestionPedidosPedidos(TestCase):
+	
+	def setUp(self):
+		test = Cliente(name='TestTapas',slug='testTapas',direccion='Pais, ciudad, calle')
+		test.save()
+		Pedido.objects.create(cliente=test, title='TestPedido', url="www.test.es", views=1, fechaPedido='02/02/16', precio='1 euro')
 
-	# def test_pedido(self):
-	# 	pedido = Pedido.objects.get(title='TestPedido')
-	# 	self.assertEqual(pedido.title, 'TestPedido')
-	# 	self.assertEqual(pedido.url, 'www.test.es')
-	# 	self.assertEqual(pedido.views, 1)
-	# 	self.assertEqual(pedido.fechaPedido, '02/02/16')
-	# 	self.assertEqual(pedido.precio, '1 euro')
+	def test_pedido(self):
+		pedido = Pedido.objects.get(title='TestPedido')
+		self.assertEqual(pedido.title, 'TestPedido')
+		self.assertEqual(pedido.url, 'www.test.es')
+		self.assertEqual(pedido.views, 1)
+		self.assertEqual(pedido.fechaPedido, '02/02/16')
+		self.assertEqual(pedido.precio, '1 euro')
+
+		
