bc6a1c9210e90a5c5fdc2eb38d1c9c62bf30313e
ignaciorecuerda <ignacio.recuerda@gmail.com>
2016-02-06 20:03:50 +0100

    añadiendo fichero fabfile

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+from fabric.api import run, local, hosts, cd
+from fabric.contrib import django
+
+#descargo el repositorio
+def descargar_repositorio():
+	run('sudo apt-get update')
+	run('sudo apt-get install -y git')
+	run('sudo git clone https://github.com/ignaciorecuerda/gestionpedidos_django.git')
+
+#Usando Docker
+#Instalando mi version de Docker
+def descargar_imagen_docker():
+	run('sudo apt-get update')
+	run('sudo apt-get install -y docker.io')
+	run('sudo docker pull ignaciorecuerda2/gestionpedidos_django')
+
+#Ejecucion de docker
+def ejecuta_docker():
+	run('sudo docker run -p 8000:8000 -i -t ignaciorecuerda2/gestionpedidos_django')
